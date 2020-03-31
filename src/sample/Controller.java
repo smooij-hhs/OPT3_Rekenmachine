47250f5f7e852dd018d4e9bde4f3ef667ae50822
Steven Mooij <s.mooij@hhs.nl>
2020-03-31 10:49:25 +0200

    Initial import.

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML private TextField txtGetal1;
+    @FXML private TextField txtGetal2;
+    @FXML private TextField txtResultaat;
+
+    @FXML
+    private void deelButtonClicked (ActionEvent event) {
+
+    }
+
+    @FXML
+    private void plusButtonClicked (ActionEvent event) {
+
+    }
+
+    @FXML
+    private void maalButtonClicked (ActionEvent event) {
+
+    }
+}
