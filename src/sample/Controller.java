10346ef5b91a6a097f921cc2101c5414752bb09f
Steven Mooij <s.mooij@hhs.nl>
2020-03-31 11:14:58 +0200

    Code voor knop toegevoegd.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,7 +12,9 @@ public class Controller {
 
     @FXML
     private void deelButtonClicked (ActionEvent event) {
-
+        int getal1 = Integer.parseInt(txtGetal1.getText());
+        int getal2 = Integer.parseInt(txtGetal2.getText());
+        int resultaat = getal1 / getal2;
     }
 
     @FXML
