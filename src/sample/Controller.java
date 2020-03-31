a8933d6039e924fb6a9655b59d7ae14f6f07f1d5
amazigh233 <45847460+amazigh233@users.noreply.github.com>
2020-03-31 11:43:38 +0200

    push knop

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,7 +19,10 @@ public class Controller {
 
     @FXML
     private void plusButtonClicked (ActionEvent event) {
-
+        int getal1 = Integer.parseInt(txtGetal1.getText());
+        int getal2 = Integer.parseInt(txtGetal2.getText());
+        int resultaat = getal1 + getal2;
+        System.out.println(resultaat);
     }
 
     @FXML
