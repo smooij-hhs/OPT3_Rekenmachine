9b852afaab1220b406d5d0393ea1be623ef5e896
tusmavan <ibrahim_zoul@live.nl>
2020-04-01 11:45:58 +0200

    groep c

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,31 +12,29 @@ public class Controller {
 
     @FXML
     private void deelButtonClicked (ActionEvent event) {
-        int getal1 = Integer.parseInt(txtGetal1.getText());
-        int getal2 = Integer.parseInt(txtGetal2.getText());
-        int resultaat = getal1 / getal2;
-        txtResultaat.setText("" + resultaat);
+        rekenKnop(1);
+
     }
 
     @FXML
     private void plusButtonClicked (ActionEvent event) {
-        int getal1 = Integer.parseInt(txtGetal1.getText());
-        int getal2 = Integer.parseInt(txtGetal2.getText());
-        int resultaat = getal1 + getal2;
-
-        String newResult = Integer.toString(resultaat);
-        txtResultaat.setText(newResult);
+        rekenKnop(2);
 
     }
 
     @FXML
     private void maalButtonClicked (ActionEvent event) {
-
+        rekenKnop(3);
     }
 
     @FXML
-    private void rekenKnop(ActionEvent event)
+    private void rekenKnop(int getal)
     {
+        int getal1 = Integer.parseInt(txtGetal1.getText());
+        int getal2 = Integer.parseInt(txtGetal2.getText());
+        if (getal == 1) {txtResultaat.setText("" + getal1 / getal2);}
+        if (getal == 2) {txtResultaat.setText("" + getal1 + getal2);}
+        if (getal == 3) {txtResultaat.setText("" + getal1 * getal2);}
 
     }
 }
