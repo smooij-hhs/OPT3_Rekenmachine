b8856cdfb8595e5da4311b6ccf32fae027a4c6c0
joepschrama <joepschrama@hotmail.com>
2020-04-01 11:46:15 +0200

    heel mooi

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,35 +2,30 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML private TextField txtGetal1;
     @FXML private TextField txtGetal2;
     @FXML private TextField txtResultaat;
-
     @FXML
-    private void deelButtonClicked (ActionEvent event) {
+    private void buttonClicked(ActionEvent event)
+    {
         int getal1 = Integer.parseInt(txtGetal1.getText());
         int getal2 = Integer.parseInt(txtGetal2.getText());
-        int resultaat = getal1 / getal2;
+        String operator = event.getSource().toString().substring(35).substring(0, 1);
+        int resultaat = 0;
+        switch(operator) {
+            case "+":
+                resultaat = getal1 + getal2;
+                break;
+            case "*":
+                resultaat = getal1 * getal2;
+                break;
+            case "/":
+                resultaat = getal1 / getal2;
+                break;
+        }
         txtResultaat.setText("" + resultaat);
-    }
-
-    @FXML
-    private void plusButtonClicked (ActionEvent event) {
-        int getal1 = Integer.parseInt(txtGetal1.getText());
-        int getal2 = Integer.parseInt(txtGetal2.getText());
-        int resultaat = getal1 + getal2;
-
-        String newResult = Integer.toString(resultaat);
-        txtResultaat.setText(newResult);
-
-    }
-
-    @FXML
-    private void maalButtonClicked (ActionEvent event) {
-
     }
 }
